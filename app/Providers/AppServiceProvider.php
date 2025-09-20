99a0cc07952683db481cc97070d663d47f96184c
dandimuzaki <dandimuzaki@gmail.com>
2025-09-20 22:51:56 +0700

    Improve update profile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('database.default') === 'sqlite') {
-            DB::statement('PRAGMA foreign_keys = ON;');
-        }
-    
-        Schema::defaultStringLength(191);    }
+       
+    }
 }
