feef0c66234744eb3a3872822a446425d8cdaff3
dandimuzaki <dandimuzaki@gmail.com>
2025-09-21 18:04:12 +0700

    Improve search post and user

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,10 @@ export default defineConfig({
         }),
     ],
     build: {
-        manifest: true,
         outDir: 'public/build',
-        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+          input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
       },
 });
