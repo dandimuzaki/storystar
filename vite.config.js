85a789a4b6a4b06de0b783e4a0f19b5474c8aab3
dandimuzaki <dandimuzaki@gmail.com>
2026-05-01 21:30:51 +0700

    refactor: modify deployment setting

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     ],
     build: {
         outDir: 'public/build',
-        manifest: true,
+        manifest: 'manifest.json',
         rollupOptions: {
           input: ['resources/css/app.css', 'resources/js/app.js'],
         },
