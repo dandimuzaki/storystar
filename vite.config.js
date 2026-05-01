89a053ccaa1806055bbbb132d441a3fdb95ef403
dandimuzaki <dandimuzaki@gmail.com>
2026-05-01 21:15:41 +0700

    refactor: modify deployment setting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -10,9 +7,6 @@ export default defineConfig({
     ],
     build: {
         outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-          input: ['resources/css/app.css', 'resources/js/app.js'],
-        },
-      },
+        manifest: 'manifest.json',
+    },
 });
